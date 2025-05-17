932bfa50a84d4b906f2255e9c65f0e36670664a9
LucChapman <lucchapman@oakland.edu>
2025-05-17 12:27:24 -0400

    Added Title Screen Scene and made that the default scene when launching application

---
+++ TitleScreenPack/title_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+#Link containers with new variables
+@onready var play_button = $ButtonControlContainer/VBoxContainer/Play
+@onready var exit_button = $ButtonControlContainer/VBoxContainer/Quit
+
+func _ready():
+	#Connect variables with actions/functions
+	play_button.pressed.connect(_on_play_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
+
+func _on_play_pressed():
+	# Replace the scene link with next game scene
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
