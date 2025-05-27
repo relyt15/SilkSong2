b15dfa746c4ccdc2469785999fdc3d54e1af90fa
LucChapman <lucchapman@oakland.edu>
2025-05-27 09:17:05 -0400

    Added Double Jump Item
    Updated player script to handle double jumping and added collectable item for double jumping.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,14 +3,28 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+#Variables for double_jump
+var collected_item_count: int = 0
+var can_double_jump: bool = false
+var jump_count: int = 0
+var max_jumps: int = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+#resets jumps when grounded
+	if is_on_floor():
+		jump_count = 0
+#allows for double jumping when items are collected
+	max_jumps = 2 if can_double_jump else 1
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions
 	var direction := Input.get_axis("ui_left", "ui_right")
--- scripts/item.gd
+++ scripts/item.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var item_name: String = "Collectable"
+@export var item_name: String = "double_jump_item"
 
 func _ready():
 	body_entered.connect(_on_body_entered)
@@ -8,4 +8,7 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		body.collected_item_count += 1
+		if body.collected_item_count >= 3 and !body.can_double_jump:
+			body.can_double_jump = true
 		queue_free()
