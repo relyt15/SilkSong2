8d3de2308e1cf513fb5223b65b4fe8d1321d7a6e
relyt15 <134524170+relyt15@users.noreply.github.com>
2025-06-17 19:59:56 -0400

    add generic music and coin sound effect.

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var item_name: String = "double_jump_item"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	body_entered.connect(_on_body_entered)
@@ -11,4 +12,4 @@ func _on_body_entered(body):
 		body.collected_item_count += 1
 		# this function does not exist in the player API
 		body.update_coin_label()
-		queue_free()
+		animation_player.play("pickup")
